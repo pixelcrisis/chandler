70575538c6b0bd0e567c4e767b3b3e038953fb66
12px <dalton@12px.io>
2019-07-03 23:39:03 -0500

    Add Prefix Change Command.

--- plugins/basics.js
+++ plugins/basics.js
@@ -2,10 +2,22 @@
 // Moves X messages from one channel
 // to a different channel.
 
+const DB   = require('../utils/db.js')
 const Util = require('../utils/util.js')
 const lang = require('../data/lang.json').basics
 
 module.exports = {
+
+  free: ['help', 'invite'],
+
+  help: function(msg, opts) {
+    return msg.channel.send({
+      embed: {
+        title: "Chandler Help",
+        description: lang.help
+      }
+    })
+  },
 
   invite: function(msg, opts) {
     return msg.channel.send({
@@ -14,6 +26,17 @@ module.exports = {
         description: lang.invite
       }
     })
+  },
+
+  prefix: function(msg, opts) {
+    if (!opts) {
+      let response = Util.parse(lang.usePrefix)
+      return msg.channel.send(response)
+    }
+
+    DB.set(msg.guild.id, 'prefix', opts.join(' '))
+    let response = Util.parse(lang.setPrefix, opts.join(' '))
+    msg.channel.send(response)
   }
 
 }
