3ec11d3cf11877862d2993701d5eb1234aff29a0
12px <dalton@12px.io>
2019-07-12 18:33:12 -0500

    Add roles command.

---
+++ commands/system/roles.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+  name: 'roles',
+  // this is the trigger for the command
+  
+  level: 5,
+
+  help: {
+    name: "{pre}roles",
+    desc: "Returns server roles + ids."
+  },
+
+  fire: function(Bot, msg, opts, lvl) {
+    let list = [], roles = msg.guild.roles.array()
+    for (var i = 0; i < roles.length; i++) {
+      list.push("`" + roles[i].id + "` - " + roles[i].name)
+    }
+    return Bot.listReply(msg, "Server Roles", list)
+  },
+
+  test: async function(Bot, msg, data) {
+    Bot.reply(msg, {
+      name: "Testing {pre}roles",
+      desc: "`{pre}roles` - list",
+      color: 16549991
+    })
+    
+    await this.fire(Bot, msg, [])
+
+    return Bot.reply(msg, "{pre}roles test complete.")
+  }
+
+}
