076464ce5d360643217eea89bdfbe4e064b1c2bb
Dalton Gore <dalton@12px.io>
2019-07-22 10:40:11 -0500

    Typo.

--- commands/admin/details.js
+++ commands/admin/details.js
@@ -13,7 +13,7 @@ module.exports = {
 
   fire: function(Bot, msg, opts, lvl) {
 
-    Bot.exec('pm2 list', (err, yay, nay) => {
+    exec('pm2 list', (err, yay, nay) => {
       if (err) return Bot.reply(msg, 'Nay')
       yay = yay.split('online')[1].split('root')[0]
       yay = yay.replace(/ +(?= )/g,'').split('│').join(' - ')
@@ -23,7 +23,7 @@ module.exports = {
         `Total Guilds: ${Bot.guilds.keyArray().length}`
       ])
     })
-    
+
   },
 
   test: async function(Bot, msg, data) {
