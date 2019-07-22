903148d7c819dc6ebda6faf69721e97c265587aa
Dalton Gore <dalton@12px.io>
2019-07-22 13:28:42 -0500

    Add reboot command.

---
+++ commands/system/reboot.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+  name: 'reboot',
+  
+  level: 9,
+
+  help: {
+    name: "{pre}reboot",
+    desc: "Reboots the bot."
+  },
+
+  fire: function(Bot, msg, opts, lvl) {
+
+    msg.react('✅')
+    Bot.log("Going Dark.")
+    Bot.exec('pm2 restart 0', (err, yay, nay) => {
+      if (err) return Bot.reply(msg, 'Nay' + Bot.clean(nay))
+      return msg.channel.send(Bot.clean(yay))
+    })
+
+  },
+
+  test: async function(Bot, msg, data) {
+    Bot.reply(msg, "No tests.")
+  }
+
+}
