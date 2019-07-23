c5ae4640e26053af414d643f6e35a1fe0caf4663
Dalton Gore <dalton@12px.io>
2019-07-23 02:54:53 -0500

    Roll onjoin into set

--- commands/admin/set.js
+++ commands/admin/set.js
@@ -7,10 +7,9 @@ module.exports = {
   lang: {
     prefix: "Set Prefix to `{val1}`\nReminder: You can @Chandler as a prefix if something goes sideways.",
     modsID: "Set Mods to <@&{val1}>",
-    warnings: {
-      on: "Enabled Command Permission Warnings.",
-      off: "Disabled Command Permission Warnings."
-    }
+    onjoin: "Logging User Joins in <#{val1}> with `{val2}`",
+    enabled: "Disabled {val1}",
+    disabled: "Disabled {val1}"
   },
 
   help: {
@@ -42,27 +41,56 @@ module.exports = {
     if (opt == 'warnings' || opts == 'warning') {
       if (Bot.yes.includes(val.toLowerCase())) {
         Bot.setConf(msg.guild.id, 'warnings', true)
-        return Bot.reply(msg, this.lang.warnings.on)
+        return Bot.reply(msg, this.lang.enabled, 'Command Permission Warnings')
       }
       if (Bot.no.includes(val.toLowerCase())) {
         Bot.setConf(msg.guild.id, 'warnings', false)
-        return Bot.reply(msg, this.lang.warnings.off)
+        return Bot.reply(msg, this.lang.disabled, 'Command Permission Warnings')
       }
     }
+
+    const getKey = val.split(' ')
+    let key = getKey.shift().toLowerCase()
+    let str = getKey.join(' ')
+
+    if (opt == 'onjoin') {
+      if (Bot.no.includes(key.toLowerCase())) {
+        Bot.setConf(msg.guild.id, 'onjoin', false)
+        return Bot.reply(msg, this.lang.disabled, 'onjoin')
+      }
+      const channel = Bot.verifyChannel(msg, key)
+      if (!channel) return Bot.reply(msg. Bot.lang.badChan, key)
+      str = str ? str : '{user} joined.'
+      Bot.setConf(msg.guild.id, 'onjoin', { channel: channel.id, message: str })
+      return Bot.reply(msg, this.lang.onjoin, channel.id, Bot.escape(str))
+    }
+
+    return Bot.reply(msg, this.help)
   },
 
   test: async function(Bot, msg, data) {
     Bot.reply(msg, {
       name: "Testing {pre}set",
       desc: "`{pre}set` - Help\n" +
-            "`{pre}set prefix ~` - Set" +
-            "`{pre}set mods developer` - Set",
+            "`{pre}set prefix ~` - Set\n" +
+            "`{pre}set mods developer` - Set\n" +
+            "`{pre}set warnings off` - Set\n" +
+            "`{pre}set onjoin test` - Set\n" +
+            "`{pre}set onjoin test message` - Set\n" +
+            "`guildMemberAdd() - message\n" +
+            "`{pre}set onjoin off` - Disabled",
       color: 16549991
     })
 
     await this.fire(Bot, msg, [])
     await this.fire(Bot, msg, ['prefix', '~'])
     await this.fire(Bot, msg, ['mods', 'developer'])
+    await this.fire(Bot, msg, ['warnings', 'off'])
+    await this.fire(Bot, msg, ['onjoin', data.channel])
+    await this.fire(Bot, msg, ['onjoin', data.channel, '{user} joined with {user.id}'])
+    await Bot.emit('guildMemberAdd', msg.member)
+    await Bot.sleep(2000)
+    await this.fire(Bot, msg, ['onjoin', 'off'])
 
     return Bot.reply(msg, "{pre}set test complete.")
   }
