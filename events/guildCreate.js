1c9c2ce61e7595e22c4af1cb93cc3e36434983b0
Dalton Gore <dalton@12px.io>
2019-07-30 17:07:58 -0500

    Initial website dev start.

--- events/guildCreate.js
+++
@@ -1,8 +0,0 @@
-// New Guild Event
-
-module.exports = async (Bot, guild) => {
-
-  let guilds = Bot.guilds.keyArray()
-  Bot.log(`Added to **${guild.name}** - Now In ${guilds.length} Servers.`)
-
-}
