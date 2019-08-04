e62e06330e767b8115224e656b265e09a2e409c6
Dalton Gore <dalton@12px.io>
2019-08-04 17:06:41 -0500

    Hotfix for guildCreate with falsy value

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -3,14 +3,13 @@
 module.exports = async (Bot, guild, test) => {
 
   const cmds = Bot.$getConf({ guild }, 'logged')
-  const desc = cmds.length ? `Last Commands: \`${cmds.join('`, `')}\`` : ''
 
   const msg = {
     color: 16736084,
     icon: guild.iconURL,
     foot: "Now In {guilds} Servers.",
     name: "Removed from {guild.name} ({guilds})",
-    desc
+    desc: cmds && cmds.length ? `Last Ran: \`${cmds.join('`, `')}\`` : ''
   }
 
   if (!test) Bot.$remConf(guild.id)
